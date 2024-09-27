556e959af1b8467435da081db0513ff5f1f7bf1e
Neelx <indraneelpatil78@gmail.com>
2024-09-27 08:17:46 +0530

    Camp commit

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+const CampSite = () => {
+  return(
+    <div>
+      Neelxxxx area 1
+    </div>
+  )
+}
 
 const Camp = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='border-2 border-green-500 2x1:max-contianer
+    relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+      <div className='hiide-scrollbar flex h-[340px] w-full items-start
+      justiffy-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[600px]'>
+        <CampSite/>
+
+      </div>
+    </section>
   )
 }
 
