9a8c1ae9402d30a000a299b9c88b561183975324
Neelx <indraneelpatil78@gmail.com>
2024-10-05 00:02:37 +0530

    get app

--- components/Features.tsx
+++ components/Features.tsx
@@ -7,7 +7,7 @@ const Features = () => {
   return (
     <section className='flex-col flexCenter 
     overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-      <div className='max-container padding-container relative w-full justify-end'>
+      <div className='max-container padding-container relative w-full flex justify-end'>
         <div className='flex flex-1 lg:min-h-[900px]'>
           <Image
             src="/phone.png"
@@ -26,7 +26,7 @@ const Features = () => {
               className='absolute left- [-5px] top-[-28px] w-10 lg:w-[50px]'/>
               <h2 className='bold-40 lg:bold-64'>Our Features</h2>
           </div>
-          <ul>
+          <ul className='mt-10 grid gap-10 md:gird-cols-2 lg:mg-20 lg:gap-20'>
             {FEATURES.map((feature)=>(
               <FeatureItem 
               key={feature.title}
@@ -53,7 +53,7 @@ type FeatureItem = {
 const FeatureItem = ({ title, icon, description }:FeatureItem) =>{
   return(
     <li className='flex w-full flex-1 flex-col items-start'>
-      <div className='rounded-full p-4 lg:p-7'>
+      <div className='rounded-full p-4 lg:p- bg-green-50'>
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
       <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const GetApp = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='flexCenter w-full flex-col pb-[1000px]'>
+      <div className='get-app'>
+        <div className='z-20 flex w-full flex-1 flex-col items-start
+        justify-center gap-12'>
+          <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>
+            Get for free now
+          </h2>
+          <p>
+            
+          </p>
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
