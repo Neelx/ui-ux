5f5d12fa9a6f6c213518fc88636aea32298e5acf
Neelx <indraneelpatil78@gmail.com>
2024-10-04 22:19:53 +0530

    features

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Features = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='border border-red-500 flex-col flexCenter 
+    overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+      <div className='max-container padding-container relative w-full justify-end'>
+        <div>
+          <Image
+            src="/photo.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"/>
+          
+        </div>
+      </div>
+
+    </section>
   )
 }
 
