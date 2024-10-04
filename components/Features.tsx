bff92b9e445ffafce793e1618a7b84a0eba9e876
Neelx <indraneelpatil78@gmail.com>
2024-10-04 22:57:03 +0530

    Featureitem

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import Image from 'next/image'
+import { FEATURES } from '@/app/constants'
+import { title } from 'process'
 
 const Features = () => {
   return (
     <section className='border border-red-500 flex-col flexCenter 
     overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
       <div className='max-container padding-container relative w-full justify-end'>
-        <div>
+        <div className='flex flex-1 lg:min-h-[900px]'>
           <Image
-            src="/photo.png"
+            src="/phone.png"
             alt="phone"
             width={440}
-            height={1000}
+            height={100}
             className="feature-phone"/>
-          
+        </div>
+        <div className='z-20 flex w-full flex-col lg:w-[60%]'>
+          <div className='relative'>
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className='absolute left- [-5px] top-[-28px] w-10 lg:w-[50px]'/>
+              <h2 className='bold-40 lg:bold-64'>Our Features</h2>
+          </div>
+          <ul>
+            {FEATURES.map((feature)=>(
+              <FeatureItem />
+            ))}
+          </ul>
         </div>
       </div>
 
@@ -21,4 +38,12 @@ const Features = () => {
   )
 }
 
+
+const FeatureItem = () =>{
+  return(
+    <div>
+      {title}
+    </div>
+  )
+}
 export default Features
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <h1>Footer will do today just to commit</h1>
+      <h1>Footer will </h1>
     </div>
   )
 }
