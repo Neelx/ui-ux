bf9996939105c048700f61ece9c549eb625600d8
Neelx <indraneelpatil78@gmail.com>
2024-10-09 22:47:13 +0530

    ts edit

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -22,7 +22,7 @@ export const NAV_LINKS = [
       icon: '/map.svg',
       variant: 'green',
       description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+        'The text has been edited will edit the other soon',
     },
     {
       title: 'Set an adventure schedule',
